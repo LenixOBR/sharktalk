239df212e5405a3d4d0802d6d6c9e1ee080d5bee
Mircas001 <mircas001@gmail.com>
2026-01-22 13:36:25 -0300

    add a debug of what the shark is speaking, for, in the future, allowing for shark TTS

--- src/components/SharkAvatar.jsx
+++ src/components/SharkAvatar.jsx
@@ -1,7 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import sharkAvatar from '../assets/placeholder.svg';
 
 const SharkAvatar = ({ message }) => {
+  useEffect(() => {
+    const handleSharkSpeak = () => {
+      if (message) {
+        console.log("Nova mensagem do tubarão:", message);
+        // Aqui você pode adicionar a função para fazer o tubarão falar
+      }
+    };
+
+    // Expõe a função no window para acessar do console
+    window.sharkSpeak = handleSharkSpeak;
+
+    return () => {
+      delete window.sharkSpeak;
+    };
+  }, [message]);
+
   return (
     <div className="shark">
       {message && (
