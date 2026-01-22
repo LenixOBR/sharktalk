efe27f8dd8676de37aee298fe6a9004140135e51
Mircas001 <mircas001@gmail.com>
2026-01-22 14:24:37 -0300

    fix footer, no more scrollbar

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,20 +3,21 @@ import React from 'react';
 function CopyrightFooter() {
     return (
     <div style={{
-        position: 'absolute',
+        position: 'fixed',
         bottom: '0',
-        left: '50%',
-        transform: 'translateX(-50%)',
-        fontSize: '12px',
+        left: '0',
+        right: '0',
+        fontSize: '11px',
         color: '#FFFFFF',
         width: '100%',
         textAlign: 'center',
+        padding: '8px 0',
       }}>
-        <a href="https://github.com/Mircas001/sharktalk/blob/main/LICENSE" target="_blank" title="AGPL-3.0" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
+        <a href="https://github.com/Mircas001/sharktalk/blob/main/LICENSE" target="_blank" rel="noopener noreferrer" title="AGPL-3.0" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
           <small>Este site é licenciado sob a AGPLv3</small>
         </a>
         <br />
-        <a href="https://github.com/Mircas001/sharktalk/" target="_blank" title="Github" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
+        <a href="https://github.com/Mircas001/sharktalk/" target="_blank" rel="noopener noreferrer" title="Github" style={{ color: '#FFFFFF', textDecoration: 'none' }}>
           <small>Acesse o código fonte</small>
         </a>
     </div>
