106da0880e375d0a80f6e8b2bb8ff0a0fd320304
Mircas001 <mircas001@protonmail.com>
2026-02-21 15:41:58 -0300

    terminar a manuntenção

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,18 @@
-import React, { useState, useEffect, useRef } from 'react';
-import './App.css';
+import { useState } from 'react'
+import maitenanceLogo from './assets/maintenance.svg'
+import './App.css'
 
-function App() {  
- 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='logo'>
+        <img src={maitenanceLogo} className="logo" alt="logo" />
+      </div>
+      <h1>Sob manuntenção</h1>
+    </>
+  )
 }
 
-export default App;
+export default App
